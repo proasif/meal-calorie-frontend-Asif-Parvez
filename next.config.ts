e954966a32753299ffd8eb621ed859ccbfe5c50f
proasif <proasifparvez@gmail.com>
2025-06-06 11:54:50 +0530

    fix

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 // Base Next.js configuration. React strict mode helps catch potential issues in
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  // pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 };
 
 export default nextConfig;
